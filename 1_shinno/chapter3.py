387cc9390bbce78e9ed82bd15190ea6e04a91200
yfur <furuse@eom.mech.eng.osaka-u.ac.jp>
2017-03-08 14:11:24 +0900

    Read up until 3.2.3 links

--- 1_shinno/chapter3.py
+++ 1_shinno/chapter3.py
@@ -5,39 +5,54 @@ from chainer import Link, Chain, ChainList
 import chainer.functions as F
 import chainer.links as L
 
-x1 = Variable(np.array([1]).astype(np.float32))
-x2 = Variable(np.array([2]).astype(np.float32))
-x3 = Variable(np.array([3]).astype(np.float32))
+# x1 = Variable(np.array([1]).astype(np.float32))
+# x2 = Variable(np.array([2]).astype(np.float32))
+# x3 = Variable(np.array([3]).astype(np.float32))
+#
+# z = (x1 - 2*x2 - 1)**2 + (x2*x3 - 1)**2 + 1
+# print(z.data)
+# print()
+#
+# print(x1.grad)
+# print()
+# z.backward()
+# print(x1.grad)
+# print(x2.grad)
+# print(x3.grad)
+# print()
+#
+# x = Variable(np.array([-1]).astype(np.float32))
+# print(F.sin(x).data, F.sin(x).dtype)
+# print(F.sigmoid(x).data, F.sin(x).dtype)
+# print()
+#
+#
+# x = Variable(np.array([-0.5]).astype(np.float32))
+# z = F.cos(x)
+# print(z.data)
+# print()
+# z.backward()
+# print(x.grad)
+# print((-1)*F.sin(x))
+#
+#
+# x = Variable(np.array([-1, 0, 1]).astype(np.float32))
+# z = F.sin(x)
+# z.grad = np.ones(3).astype(np.float32)
+# z.backward()
+# print(x.grad)
 
-z = (x1 - 2*x2 - 1)**2 + (x2*x3 - 1)**2 + 1
-print(z.data)
+''' 3.2.3 '''
+h = L.Linear(3, 4)
+x = Variable(np.array(range(6)).astype(np.float32).reshape(2, 3))
+print(h.W.data)
+print(x.data)
 print()
-
-print(x1.grad)
+print(h.b.data)
 print()
-z.backward()
-print(x1.grad)
-print(x2.grad)
-print(x3.grad)
-print()
+y = h(x)
+print(y.data)
 
-x = Variable(np.array([-1]).astype(np.float32))
-print(F.sin(x).data, F.sin(x).dtype)
-print(F.sigmoid(x).data, F.sin(x).dtype)
-print()
-
-
-x = Variable(np.array([-0.5]).astype(np.float32))
-z = F.cos(x)
-print(z.data)
-print()
-z.backward()
-print(x.grad)
-print((-1)*F.sin(x))
-
-
-x = Variable(np.array([-1, 0, 1]).astype(np.float32))
-z = F.sin(x)
-z.grad = np.ones(3).astype(np.float32)
-z.backward()
-print(x.grad)
+w = h.W.data
+x0 = x.data
+print(x0.dot(w.T) + h.b.data)
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Deep Learning with Chainer
 This repository is for my private study of deep learning with [Chainer](http://chainer.org/).
 
-* 1_shinno: *Chainerによる実践深層学習 (Deepl Learning with Chainer)* by 新納浩幸 (Hiroyuki Shinno)
+* 1_shinno: Coding with the textbook; *Chainerによる実践深層学習 (Deepl Learning with Chainer)* by 新納浩幸 (Hiroyuki Shinno)
